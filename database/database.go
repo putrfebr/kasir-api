ef6e795a3c981316b5be5e837897a02ac0fa64c6
Putri Febri Nuraini <putripfn46@gmail.com>
2026-02-02 06:17:54 +0800

    layered arch, env, database

--- database/database.go
+++ database/database.go
@@ -3,27 +3,30 @@ package database
 import (
 	"database/sql"
 	"log"
+	"strings"
 
 	_ "github.com/lib/pq"
 )
 
-func InitDB(connectionString string) (*sql.DB, error) {
-	// Open database
-	db, err := sql.Open("postgres", connectionString)
+func InitDB(dsn string) (*sql.DB, error) {
+	dsn = strings.TrimSpace(dsn)
+
+	db, err := sql.Open("postgres", dsn)
 	if err != nil {
 		return nil, err
 	}
 
+	// Test koneksi (aman untuk transaction pooler)
 	// Test connection
 	err = db.Ping()
 	if err != nil {
 		return nil, err
 	}
 
-	// Set connection pool settings (optional tapi recommended)
+	// Connection pool settings
 	db.SetMaxOpenConns(25)
 	db.SetMaxIdleConns(5)
 
-	log.Println("Database connected successfully")
+	log.Println("Database connected successfully ✅")
 	return db, nil
 }
